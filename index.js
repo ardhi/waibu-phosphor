e4b99d2eafedc1075bf695c705172070b8ba8580
Ardhi Lukianto <ardhi@lukianto.com>
2025-09-28 21:22:35 +0700

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+/**
+ * Plugin factory
+ *
+ * @param {string} pkgName - NPM package name
+ * @returns {class}
+ */
 async function factory (pkgName) {
   const me = this
 
+  /**
+   * WaibuPhosphor class
+   *
+   * @class
+   */
   class WaibuPhosphor extends this.app.pluginClass.base {
     static alias = 'wpp'
     static dependencies = ['waibu-mpa']
